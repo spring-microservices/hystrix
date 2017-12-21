f9a13820791ea527c07eba017b78177fc0895334
Manu K M <manukm07@gmail.com>
2017-12-21 15:26:40 +0530

    Apply HystrixCommand failover method

--- src/main/java/org/manu/springcloud/HystrixApplication.java
+++ src/main/java/org/manu/springcloud/HystrixApplication.java
@@ -1,15 +1,19 @@
 package org.manu.springcloud;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
+@EnableCircuitBreaker
 @RestController
 @SpringBootApplication
 public class HystrixApplication {
@@ -22,9 +26,14 @@ public class HystrixApplication {
 	@Autowired
 	private RestTemplate restTemplate;
 
+	@HystrixCommand(fallbackMethod = "failover")
 	@GetMapping("/startClient")
 	public List startClient(){
 		return this.restTemplate.getForObject("http://localhost:8888/service", List.class);
+	}
+
+	private List<String> failover(){
+		return Arrays.asList("Default1", "Default2");
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(HystrixApplication.class, args);
