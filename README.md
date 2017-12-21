b4cd6cc69594d6fd777188735907512b24d19b8a
Manu K M <manukm07@gmail.com>
2017-12-21 16:54:44 +0530

    Adding timeout to api call to fallback to failover method

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Histrix demo
+
+See [Hystric configuration](https://github.com/Netflix/Hystrix/wiki/Configuration "Hystrix Config").
--- src/main/java/org/manu/springcloud/HystrixApplication.java
+++ src/main/java/org/manu/springcloud/HystrixApplication.java
@@ -1,6 +1,7 @@
 package org.manu.springcloud;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,24 +19,28 @@ import java.util.List;
 @SpringBootApplication
 public class HystrixApplication {
 
-	@Bean
-	public RestTemplate restTemplate(){
-		return new RestTemplate();
-	}
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
-	@Autowired
-	private RestTemplate restTemplate;
+    @Autowired
+    private RestTemplate restTemplate;
 
-	@HystrixCommand(fallbackMethod = "failover")
-	@GetMapping("/startClient")
-	public List startClient(){
-		return this.restTemplate.getForObject("http://localhost:8888/service", List.class);
-	}
+    @HystrixCommand(fallbackMethod = "failover", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500")
+    })
+    @GetMapping("/startClient")
+    public List startClient(long time) throws InterruptedException {
+        Thread.sleep(time);
+        return this.restTemplate.getForObject("http://localhost:8888/service", List.class);
+    }
 
-	private List<String> failover(){
-		return Arrays.asList("Default1", "Default2");
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(HystrixApplication.class, args);
-	}
+    private List<String> failover(long time) {
+        return Arrays.asList("Default1", "Default2");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixApplication.class, args);
+    }
 }
